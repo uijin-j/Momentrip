5fc84342a138d80ce9965c59edf07124d1373ee3
유서린 <81281190+SeorinY@users.noreply.github.com>
2021-08-04 10:33:26 +0900

    user-file create
    Create file
    auth.js
    middlewares.js
    user.js

--- MomentripBack/app.js
+++ MomentripBack/app.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const userRouter = require('./routes/user');
 
 //시퀄라이즈 문
 const { sequelize } = require('./models');
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

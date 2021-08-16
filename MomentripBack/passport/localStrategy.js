affdff351785af6c7fbefa469301694d6f4c5be5
mk1604 <btob6461@naver.com>
2021-08-16 11:43:18 +0900

    Revert "passport login"
    This reverts commit 3dbd54bb514f1fefbb0f4fa9a4f71c09eb62b0d6.

--- MomentripBack/passport/localStrategy.js
+++ MomentripBack/passport/localStrategy.js
@@ -1,8 +1,13 @@
-const passport = require('passport');
+// const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
+
 const User = require('../models/user');
+
+//실험
 require('dotenv').config();
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
 const { ExtractJwt, Strategy: JWTStrategy } = require('passport-jwt');
 
 const JWTConfig = {
@@ -10,23 +15,24 @@ const JWTConfig = {
     secretOrKey: process.env.JWT_SECRET_KEY,
 };
 
+//jwt 토큰 안증 검증하기
 const JWTVerify = async (jwtPayload, done) => {
     try {
-        console.log("JWTVerify 실행됨 ");
+        jwtPayload
         // jwtPayload에 유저 정보가 담겨있다.
-        // // 해당 정보로 유저 식별 로직을 거친다.
-        let user = User.findOne({where : {email : jwtPayload.email }})
+        // 해당 정보로 유저 식별 로직을 거친다.
+
+
         // 유효한 유저라면
         if (user) {
-            console.log("Token Verify");
-            return done(null, user, {message : "token Verify"});
+            done(null, user);
+            return;
         }
         // 유저가 유효하지 않다면
-        console.log("Token is not Verify");
-        return done(null, false, { message: 'inaccurate token.' });
+        done(null, false, { message: 'inaccurate token.' });
     } catch (error) {
-            return res.status(400).error(error);
-
+        console.error(error);
+        done(error);
     }
 };
 passport.use('jwt', new JWTStrategy(JWTConfig, JWTVerify));
@@ -35,11 +41,27 @@ passport.use('jwt', new JWTStrategy(JWTConfig, JWTVerify));
 const passportConfig = { usernameField: 'email', passwordField : 'password' };
 
 passport.use(
+    //회원가입이므로 볼 필요 x
+    'signup',
+    new localStrategy(passportConfig, async (userName, password, done) => {
+        // 유저 생성
+        // 성공하면
+        return done(null, userName);
+
+        // 실패하면
+        return done(null, false, { message: 'User creation fail.' });
+    })
+);
+// passport.use('minusSign', localStrategy(passportConfig,async (User)));
+
+
+passport.use(
     'signin',
-    new LocalStrategy(passportConfig, async (email, password, done) => {
-        JWTStrategy(JWTConfig, JWTVerify);
+    new localStrategy(passportConfig, async (email, password, done) => {
+        // console.log("passport user signin");
         let user = await User.findOne({where: {email}})
         if(user){
+            // 유저가 db 에 존재한다면
             const result = await bcrypt.compare(password, user.password);
             if(result){
                 return done(null, user, { message: 'Sign in Successful' });
@@ -53,3 +75,33 @@ passport.use(
 );
 
 module.exports = passport ;
+//여기까지
+
+
+
+/*
+module.exports = () => {
+    console.log("local asdasdasdasdadsasd 실행");
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+    }, async (email, passport, done) => {
+        try{
+            const exUser = await User.findOne({ where: { email } });
+            if (exUser) {
+                const result = await bcrypt.compare(password, exUser.password);
+                if (result) {
+                    done(null, exUser);
+                } else {
+                    done(null, false, { message: '비밀번호가 일치하지 않습니다.' });
+                }
+            } else {
+                done(null, false, { message: '가입되지 않은 회원입니다.' });
+            }
+        }catch(error){
+            console.error(error);
+            done(error);
+        }
+    }));
+};
+*/
--- MomentripBack/routes/auth.js
+++ MomentripBack/routes/auth.js
@@ -7,25 +7,44 @@ const {local} = require('../passport/index')
 
 const router = express.Router();
 
+
+//실험
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-router.post('/signIn' ,(req, res, next) => {
+router.post('/login1', isNotLoggedIn ,(req, res, next) => {
+    console.log("post /auth/login1");
     passport.authenticate('signin', (err, user, info) => {
+        console.log(user);
         if (!user) {
             return res.status(400).json({ message: info.message });
         }
         const token = jwt.sign(
-            { Email : req.body.email },
+            { userName: req.body.userName },
             process.env.JWT_SECRET_KEY
         );
-        return res.json({ user ,token });
+        console.log('로그인성공');
+        res.json({ token });
     })(req, res, next);
 });
+//
+// router.post('/login2', async (req, res, next) => {
+//     let {email, password} = req.body;
+//     console.log(email, " + ", password);
+//     var user = await User.findOne({where: { email : email}})
+//     // console.log(user.email)
+//     // console.log(user.name);
+//     console.log(user);
+//     return res.status(200).json(user);
+// })
+//여기까지
 
-router.post('/signUp', async (req,res,next) => {
 
+
+
+router.post('/join',isNotLoggedIn, async (req,res,next) => {
     const { email, password, nick, name } = req.body;
+
     try{
         const exUser = await User.findOne({where: {email}});
         if(exUser){
@@ -44,5 +63,27 @@ router.post('/signUp', async (req,res,next) => {
         return next(error);
     }
 });
+//
+//
+// router.post('/login', isNotLoggedIn, (req,res,next) => {
+//     console.log("post login");
+//     passport.authenticate('local', (authError, user, info) => {
+//         if (authError) {
+//             console.error(authError);
+//             return next(authError);
+//         }
+//         if (!user) {
+//             return res.redirect(`/?loginError=${info.message}`);
+//         }
+//         return req.login(user, (loginError) => {
+//             if (loginError) {
+//                 console.error(loginError);
+//                 return next(loginError);
+//             }
+//             return res.redirect('/');
+//         });
+//     })(req, res, next);
+// });
+//
 
 module.exports = router;

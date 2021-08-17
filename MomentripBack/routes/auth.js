86d98bf9d7596c34001aa3e5269a30560425bec7
유서린 <81281190+SeorinY@users.noreply.github.com>
2021-08-17 14:18:33 +0900

    recovery code
    recovery kakao router, strategy

--- MomentripBack/routes/auth.js
+++ MomentripBack/routes/auth.js
@@ -7,44 +7,25 @@ const {local} = require('../passport/index')
 
 const router = express.Router();
 
-
-//실험
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-router.post('/login1', isNotLoggedIn ,(req, res, next) => {
-    console.log("post /auth/login1");
+router.post('/signIn' ,(req, res, next) => {
     passport.authenticate('signin', (err, user, info) => {
-        console.log(user);
         if (!user) {
             return res.status(400).json({ message: info.message });
         }
         const token = jwt.sign(
-            { userName: req.body.userName },
+            { Email : req.body.email },
             process.env.JWT_SECRET_KEY
         );
-        console.log('로그인성공');
-        res.json({ token });
+        return res.json({ user ,token });
     })(req, res, next);
 });
-//
-// router.post('/login2', async (req, res, next) => {
-//     let {email, password} = req.body;
-//     console.log(email, " + ", password);
-//     var user = await User.findOne({where: { email : email}})
-//     // console.log(user.email)
-//     // console.log(user.name);
-//     console.log(user);
-//     return res.status(200).json(user);
-// })
-//여기까지
 
+router.post('/signUp', async (req,res,next) => {
 
-
-
-router.post('/join',isNotLoggedIn, async (req,res,next) => {
     const { email, password, nick, name } = req.body;
-
     try{
         const exUser = await User.findOne({where: {email}});
         if(exUser){
@@ -63,28 +44,6 @@ router.post('/join',isNotLoggedIn, async (req,res,next) => {
         return next(error);
     }
 });
-//
-//
-// router.post('/login', isNotLoggedIn, (req,res,next) => {
-//     console.log("post login");
-//     passport.authenticate('local', (authError, user, info) => {
-//         if (authError) {
-//             console.error(authError);
-//             return next(authError);
-//         }
-//         if (!user) {
-//             return res.redirect(`/?loginError=${info.message}`);
-//         }
-//         return req.login(user, (loginError) => {
-//             if (loginError) {
-//                 console.error(loginError);
-//                 return next(loginError);
-//             }
-//             return res.redirect('/');
-//         });
-//     })(req, res, next);
-// });
-//
 
 
 router.get('/kakao', passport.authenticate('kakao'));

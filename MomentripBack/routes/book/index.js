42b23d36985d0f3ac776def29d73a789af8025d9
유서린 <81281190+SeorinY@users.noreply.github.com>
2021-09-03 18:32:30 +0900

    Modify book router
    deleted startDate, updateDate
    update book router

--- MomentripBack/routes/book/index.js
+++ MomentripBack/routes/book/index.js
@@ -7,8 +7,8 @@ router.post('/', bookController.registerBook); // book 만들기
 router.get('/', bookController.findAllBook) //book 모두 불러오기
 router.get('/select/:id', bookController.findBookById) // 특정 book 불러오기
 router.get('/user/all/:user_id', bookController.searchBookByUserId()) //특정 유저의 book 보기
-router.update('/:id', ) // 특정 book 업데이트
-router.delete('/:id') // 특정 book 삭제
+router.update('/:id', bookController.updateBookById ) // 특정 book 업데이트
+router.delete('/:id', bookController.deleteBook) // 특정 book 삭제
 
 
 module.exports = router;

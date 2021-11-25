ad1e38d3915ab5fb1f347ba8ac58b6a216cd27d5
유서린 <81281190+SeorinY@users.noreply.github.com>
2021-11-25 12:24:35 +0900

    datebase restart
    drop database
    recreate database true

--- MomentripBack/app.js
+++ MomentripBack/app.js
@@ -27,8 +27,8 @@ nunjucks.configure('views', {
     express: app,
     watch: true,
 });
-// sequelize.sync({ force: true})
-sequelize.sync({ force: false})
+sequelize.sync({ force: true})
+// sequelize.sync({ force: false})
     .then(() => {
         console.log('데이터베이스 연결 성공');
     })
